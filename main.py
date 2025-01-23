93ed0ac4dcf0f87c45f91af9d884506ff82d53c4
lithinglol <lcfa0725@icloud.com>
2025-01-22 18:25:37 -0600

    Update main.py
    Se añadió el comando ayuda y se cambio el prefijo a "+"

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='+', intents=intents)
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
+    await ctx.send(f'Hola, soy {bot.user}!')
 
 @bot.command()
 async def tipos(ctx):
@@ -50,4 +50,12 @@ async def random_stat(ctx):
         await ctx.send("En México, se recolectan diariamente 103 millones de kilogramos de residuos urbanos.")
     elif ran == 4:
         await ctx.send("En 2022, se recibieron en México 6,047 kilogramos de material eléctrico y electrónico, 5,231 kilogramos de otros materiales, 2,169 kilogramos de llantas y 1,626 kilogramos de otros plásticos.")
+
+@bot.command()
+async def ayuda(ctx):
+    with open(f'comandos.txt', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        text = discord.File(f)
+    await ctx.send(file=text)
+
 bot.run("*tu token aqui*")
